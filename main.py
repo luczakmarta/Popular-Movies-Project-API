6920141f2158493cc9bf980f9dca958b920443dc
Marta Luczak <luczakmarta1997@gmail.com>
2021-08-09 23:35:23 +0200

    Movies project

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/homepage')
+def homepage():
+    movies = [
+        {"Tittle":"Titanic"},
+        {"Tittle":"Avatar"}
+
+
+    ]
+    return render_template("homepage.html", movies=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
